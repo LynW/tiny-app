f77fc9f5a445a25a1b2591bf14a5e668e3de40ac
lynw <c54wang@gmail.com>
2022-09-06 18:11:32 -0400

    add comments

--- express_server.js
+++ express_server.js
@@ -10,46 +10,52 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//Default page that shows index page or urls
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
+//Show our list of urls page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Create the url
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
+
+//Delete the url
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
+//Show the create url page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Show information of a specific URL in our JSON
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+//Redirect to the long url
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
+//Show our urls in JSON format
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
