357e42afcb6a90107c0ff175aa8860c914c8ec3b
lynw <c54wang@gmail.com>
2022-09-09 12:58:18 -0400

    fix delete and fix perms for user that has access to link

--- express_server.js
+++ express_server.js
@@ -120,40 +120,63 @@ app.get("/urls/new", (req, res) => {
 
 //Delete the url
 app.post("/urls/:id/delete", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const user = req.cookies["user_id"];
   const userUrls = urlsForUser(urlDatabase, req.cookies["user_id"]);
-
+  const databaseOb = urlDatabase[req.params.id];
+  
+  if (!user) {
+    res.status(401).send("You must be logged in to see this page.");
+  }
   if (req.cookies["user_id"]) {
     const templateVars = {
       urls: userUrls,
       user: users[req.cookies["user_id"]]
     };
-
-  if (userId === urlDatabase[req.params.id].userID) {
-    console.log("This should delete...")
+  } 
+  if (user === urlDatabase[req.params.id].userID) {
     delete urlDatabase[req.params.id];
     res.redirect(`/urls`);
   } else {
     res.status(404).send("You do not have permision to delete");
   }
+  if (!databaseOb) {
+    res.status(404).send("URL does not exist");
   }
+  if (user !== databaseOb.userID){
+    res.status(401).send("You cannot access this.");
+  } 
+
 });
 
 //Show information of a specific URL in our JSON
+//WHEN I GET BACK - PLEASE FIX THIS SHIT
 app.get("/urls/:id", (req, res) => {
-  const urldb = urlDatabaseMapper(urlDatabase);  
+  const urldb = urlDatabaseMapper(urlDatabase);
+  const user = req.cookies["user_id"];
   const databaseOb = urldb[req.params.id];
+  const userUrls = urlsForUser(urlDatabase, req.cookies["user_id"]);
   const templateVars = {
     id: req.params.id,
     longURL: urldb[req.params.id],
-    user: users[req.cookies["user_id"]]
+    user: users[user]
   };
 
-  if (!databaseOb) {
-    res.status(404).send("<h1>Short URL does not exist</h1>");
-  } else {
-    res.render("urls_show", templateVars);
+  // console.log("user:");
+  // console.log(user)
+  // console.log("DB is:");
+  // console.log(urlDatabase[req.params.id].userID);
+
+  if (!user) {
+    res.status(401).send("You must be logged in to see this page.");
   }
+  if (!databaseOb) {
+    res.status(404).send("Short URL does not exist");
+  }
+  else if (user !== urlDatabase[req.params.id].userID){
+    res.status(401).send("You cannot access this.");
+  } 
+  res.render("urls_show", templateVars);
+
 });
 
 //Update the long URL
