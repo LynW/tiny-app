995fa9bca8a2be9f00428461de3d564dd6432ce1
lynw <c54wang@gmail.com>
2022-09-08 23:41:37 -0400

    new urlDatabase structure

--- express_server.js
+++ express_server.js
@@ -22,14 +22,33 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "user2ID"
+  },
+  Osm5xK: { 
+    longURL: "http://www.google.com",
+    userID: "user1ID"
+  }
 };
+
+const urlDatabaseMapper = function(database) {
+  let obj = {};
+  for (let data in database) {
+    obj[data] = database[data].longURL
+  }
+  return obj
+};
+
+const urldb = urlDatabaseMapper(urlDatabase);
+console.log("URL Database" , urldb);
+
+
 
 //Default page that shows index page or urls
 app.get("/", (req, res) => {
   const templateVars = { 
-    urls: urlDatabase,
+    urls: urldb,
     user: users[req.cookies["user_id"]] 
   };
   if (!req.cookies["user_id"]) {
@@ -43,7 +62,7 @@ app.get("/", (req, res) => {
 //Show our list of urls page
 app.get("/urls", (req, res) => {
   const templateVars = { 
-    urls: urlDatabase,
+    urls: urldb,
     user: users[req.cookies["user_id"]] 
   };
   if (!req.cookies["user_id"]) {
@@ -59,15 +78,9 @@ app.post("/urls", (req, res) => {
   if (!req.cookies["user_id"]) {
     res.send("Not logged in - please login to shorten URLs.");
   } else {
-  urlDatabase[shortURL] = req.body.longURL;
+  urldb[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
   }
-});
-
-//Delete the url
-app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
 });
 
 //Show the create url page
@@ -75,29 +88,50 @@ app.get("/urls/new", (req, res) => {
   const templateVars = {
     user: users[req.cookies["user_id"]]
   };
+  if (!req.cookies["user_id"]) {
+    res.redirect('/login');
+  } else {
     res.render('urls_new', templateVars);
+  }
 });
+
+//Delete the url
+app.post("/urls/:id/delete", (req, res) => {
+  delete urldb[req.params.id];
+  res.redirect('/urls');
+});
+
+
 
 //Show information of a specific URL in our JSON
 app.get("/urls/:id", (req, res) => {
+  const databaseOb = urldb[req.params.id];
   const templateVars = { 
     id: req.params.id, 
-    longURL: urlDatabase[req.params.id],
+    longURL: urldb[req.params.id],
     user: users[req.cookies["user_id"]]
   };
-  res.render("urls_show", templateVars);
+  if (!databaseOb) {
+    res.status(404).send("<h1>Short URL does not exist</h1>");
+  }
+    res.render("urls_show", templateVars);
 });
 
 //Update the long URL
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
+  urldb[req.params.id] = req.body.longURL;
   res.redirect(`/urls/${req.params.id}`);
 });
 
 //Redirect to the long url
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  const longURL = urldb[req.params.id]
+  if (!longURL) {
+    res.status(404).send("<h1>Short URL does not exist</h1>")
+    return
+  } else {
+    res.redirect(longURL);
+  }
 });
 
 //Show our urls in JSON format
@@ -114,7 +148,11 @@ app.get("/register", (req, res) => {
   const templateVars = { 
     user: users[req.cookies["user_id"]]
   };
-  res.render("urls_register", templateVars);
+  if (!req.cookies["user_id"]) {
+    res.render("urls_register", templateVars);
+  } else {
+    res.redirect('/urls');
+  }
 });
 
 //Creates user and cookie session
@@ -123,7 +161,7 @@ app.post('/register', (req, res) => {
   console.log(userExists);
   // checks if email/password are empty/email registered
   if (!req.body.email || !req.body.password || userExists) {
-    res.send("400 - Bad Request");
+    res.status(404).send("<h1>Bad Request</h1>")
   } else {
     const newUserID = generateRandomString();
     const newUser = {
@@ -137,17 +175,22 @@ app.post('/register', (req, res) => {
   }
 });
 
+//Go to login page
 app.get("/login", (req, res) => {
   const templateVars = { 
     user: users[req.cookies["user_id"]]
   };
-  res.render('urls_login', templateVars);
+  if (!req.cookies["user_id"]) {
+    res.render('urls_login', templateVars);
+  } else {
+    res.redirect('/urls');
+  }
 });
 
 app.post("/login", (req, res) => {
   const userExists = emailLookup(users, req.body.email);
   if (!userExists || users[userExists].password !== req.body.password) {
-    res.send("403 - Access Forbidden");
+    res.status(403).send("<h1>Access Forbidden</h1>")
   } else {
   res.cookie('user_id', userExists);
   res.redirect('/urls');
